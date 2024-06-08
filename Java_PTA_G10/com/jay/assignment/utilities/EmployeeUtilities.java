3509cb8a58922914a28560814b468753ade23500
jay0013 <jaychavda2003@gmail.com>
2024-06-08 15:49:56 +0530

    First Commit

---
+++ Java_PTA_G10/com/jay/assignment/utilities/EmployeeUtilities.java
@@ -0,0 +1,28 @@
+package com.jay.assignment.utilities;
+
+import com.jay.assignment.employees.Employee;
+import com.jay.assignment.employees.Manager;
+import com.jay.assignment.employees.Developer;
+
+
+public class EmployeeUtilities {
+
+    public static void increaseSalary(Employee employee, double percentage) {
+        double newSalary = employee.getSalary() * (1 + percentage / 100);
+        employee.setSalary(newSalary);
+    }
+
+    public static void printEmployeeDetails(Employee employee) {
+        System.out.println("Name: " + employee.getName());
+        System.out.println("Employee ID: " + employee.getEmployeeId());
+        System.out.println("Salary: " + employee.getSalary());
+
+        if (employee instanceof Manager) {
+            Manager manager = (Manager) employee;
+            System.out.println("Department: " + manager.getDepartment());
+        } else if (employee instanceof Developer) {
+            Developer developer = (Developer) employee;
+            System.out.println("Programming Language: " + developer.getProgrammingLanguage());
+        }
+    }
+}
